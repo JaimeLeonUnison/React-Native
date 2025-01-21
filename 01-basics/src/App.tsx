48c56cd5dfc2d4e5e3dd2ee5f1410d8b909515a1
JaimeLeonUnison <a217203271@unison.mx>
2025-01-20 23:07:45 -0700

    AuthContext-Creado

--- 01-basics/src/App.tsx
+++ 01-basics/src/App.tsx
@@ -1,17 +1,25 @@
 // import { BasicFunctions } from './typescript/BasicFunctions'
 // import { ObjectLiterals } from './typescript/ObjectLiterals'
 // import { BasicTypes } from './typescript/BasicTypes'
-import { Counter } from './components/Counter';
+// import { Counter } from './components/Counter';
+
+import { AuthProvider } from "./context/AuthContext"
+import { LoginPage } from './components/LoginPage';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center h-svh">
-      <h1 className="text-4xl mb-5">React + TS</h1>
-      {/* {<ObjectLiterals/>} */}
-      {/* {<BasicTypes/>} */}
-      {/*     <BasicFunctions/> */}
-      <Counter />
-    </div>
+
+    <AuthProvider>
+      <div className="flex flex-col justify-center items-center h-svh">
+        <h1 className="text-4xl mb-5">React + TS</h1>
+
+        {/* {<ObjectLiterals/>} */}
+        {/* {<BasicTypes/>} */}
+        {/* {<BasicFunctions/>} */}
+        {/* <Counter /> */}
+        <LoginPage />
+      </div>
+    </AuthProvider>
   )
 }
 
